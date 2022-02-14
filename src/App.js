4e60e8b559dcd9f6e83f3782728ca5020595c0c0
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-02-14 19:27:10 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useRef, useState, useEffect } from "react";
+import girl from "./Assets/girl.png";
 function App() {
   const [timerDays, setTimerDays] = useState("00");
   const [timerHours, setTimerHours] = useState("00");
@@ -37,17 +38,27 @@ function App() {
   });
   return (
     <div className="App">
-      <div className="timer-div">
-        <div className="time-box">
-          <h2>{timerHours}</h2>
-        </div>
-        <p>:</p>
-        <div className="time-box">
-          <h2>{timerMinutes}</h2>
-        </div>
-        <p>:</p>
-        <div className="time-box">
-          <h2>{timerSeconds}</h2>
+      <div className="top-app">
+        <h2>ARCANE TOKEN</h2>
+        <h1>WEBSITE LAUNCH</h1>
+      </div>
+
+      <div className="white-box">
+        <img src={girl} alt="" />
+        <h1>15 FEBURARY</h1>
+        <h1>15 UTC</h1>
+        <div className="timer-div">
+          <div className="time-box">
+            <h2>{timerHours}</h2>
+          </div>
+          <p>:</p>
+          <div className="time-box">
+            <h2>{timerMinutes}</h2>
+          </div>
+          <p>:</p>
+          <div className="time-box">
+            <h2>{timerSeconds}</h2>
+          </div>
         </div>
       </div>
     </div>
